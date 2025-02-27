41163fcbadaf993d5a189d1a8cbf0a435cdab0d9
Ethan Hofstetter <ethan.hofstetter@ceff.ch>
2025-02-27 20:00:30 +0100

    ajout de l'ajout des entitées

--- flutter_structure_creation_script.ps1
+++ flutter_structure_creation_script.ps1
@@ -1,4 +1,4 @@
-#récupérer l'environnement de flutter
+#récupérer l'environnement de flutter (sdk)
 
 # Sélection du chemin
 $path = Read-Host "Entrez le chemin de votre projet Flutter"
@@ -11,33 +11,30 @@ $name = Read-Host "Entrez le nom de votre projet Flutter"
 $theme = Read-Host "Entrez le thème principal de votre projet Flutter"
 
 # Sélection des entités du projet
+$entities = @()
+
 do {
     $entity = Read-Host "Entrez une entité (appuyez sur 'Entrée' pour terminer)"
-
-    $entities = @()
-
     if ($entity -ne "") {
-        $entities += $entity 
+        $entities += $entity
     }
 } while ($entity -ne "")
 
 # Création du dossier package
-mkdir package
-Set-Location package
+mkdir packages
+Set-Location packages
 
 # Création du dossier domain_entities
 mkdir domain_entities
 Set-Location domain_entities
 
-
-
 # Création des fichiers analysis_options.yaml, pubspec.yaml
 New-Item -Name analysis_options.yaml -ItemType file
-@("include: package:flutter_lints/flutter.yaml", "", "linter:", "  rules:") | Add-Content -Path $path\package\domain_entities\analysis_options.yaml
+@("include: package:flutter_lints/flutter.yaml", "", "linter:", "  rules:") | Add-Content -Path $path\packages\domain_entities\analysis_options.yaml
 
 New-Item -Name pubspec.yaml -ItemType file
 
-@("name: domain_entities", "publish_to: none", "", "environment:", "  sdk: ^3.5.1", "", "dev_dependencies:","  flutter_lints: ^4.0.0") | Add-Content -Path $path\package\domain_entities\pubspec.yaml
+@("name: domain_entities", "publish_to: none", "", "environment:", "  sdk: ^3.5.1", "", "dev_dependencies:","  flutter_lints: ^4.0.0") | Add-Content -Path $path\packages\domain_entities\pubspec.yaml
 
 # Ajout de la dépendance equatable
 flutter pub add equatable
@@ -49,15 +46,16 @@ Set-Location lib
 New-Item -Name domain_entities.dart -ItemType file
 
 mkdir src
+Set-Location src
 
-# Création des entités problèmes ici
+# Création des entités - problèmes ici
 foreach ($entity in $entities) {
     mkdir $entity
-    Set-Location src\$entity
+    Set-Location $entity
 
     New-Item -Name $entity.dart -ItemType file
 
-    @("class $entity {", "  const $entity({", " })", "}") | Add-Content -Path $path\package\domain_entities\lib\src\$entity\$entity.dart
+    @("class $entity {", "  const $entity({", " })", "}") | Add-Content -Path $path\packages\domain_entities\lib\src\$entity\$entity.dart
 
     Set-Location ..
 }
