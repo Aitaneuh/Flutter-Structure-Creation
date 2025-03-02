9404551607931a778bdc3b565aaabeec87853d73
Ethan Hofstetter <ethan.hofstetter@ceff.ch>
2025-03-02 20:09:05 +0100

    fin du dossier packages

--- flutter_structure_creation_script.ps1
+++ flutter_structure_creation_script.ps1
@@ -1,3 +1,4 @@
+
 #récupérer l'environnement de flutter (sdk)
 
 # Sélection du chemin
@@ -5,6 +6,7 @@
 $path = "C:/Test/test"
 Set-Location $path
 
+Remove-Item -Recurse -Force "packages"
 
 # Sélection du nom du projet
 # $name = Read-Host "Entrez le nom de votre projet Flutter"
@@ -25,7 +27,12 @@ $theme = 'voitures';
 
 $entities = @('modele', 'marque');
 
-# Création du dossier package
+# Suppression du dossier test
+if (Test-Path -Path "test") {
+    Remove-Item -Recurse -Force "test"
+}
+
+# Création du dossier packages
 mkdir packages
 Set-Location packages
 
@@ -117,7 +124,7 @@ Set-Location component_library
 # Ajout du fichier analysis_options.yaml et pubspec.yaml
 @("include: package:flutter_lints/flutter.yaml", "", "linter:", "  rules:") | Add-Content -Path $path\packages\component_library\analysis_options.yaml
 
-@("name: component_library", "publish_to: none", "", "environment:", "  sdk: ^3.5.1", "", "dependencies:", "  flutter:", "    sdk: flutter", "  domain_entities:", "    path: ../domain_entities", "dev_dependencies:", "  flutter_lints: ^4.0.0", "flutter:", "  uses-material-design: true") | Add-Content -Path $path\packages\component_library\pubspec.yaml
+@("name: component_library", "publish_to: none", "", "environment:", "  sdk: ^3.5.1", "", "dependencies:", "  flutter:", "    sdk: flutter", "  domain_entities:", "    path: ../domain_entities", "dev_dependencies:", "  flutter_lints: ^4.0.0") | Add-Content -Path $path\packages\component_library\pubspec.yaml
 
 # Ajout du dossier lib et du fichier component_library.dart
 mkdir lib
@@ -127,3 +134,54 @@ New-Item -Name component_library.dart -ItemType file
 
 mkdir src
 
+# Ajout de la variable du nom de la "list"
+$nameList = $theme + "_list";
+
+# Création du dossier features et du sous-dossier list
+Set-Location $path\packages
+mkdir features
+Set-Location features
+
+mkdir $nameList
+Set-Location $nameList
+
+# Ajout du fichier analysis_options.yaml et pubspec.yaml
+New-Item -Name analysis_options.yaml -ItemType file
+@("include: package:flutter_lints/flutter.yaml", "", "linter:", "  rules:") | Add-Content -Path $path\packages\features\$nameList\analysis_options.yaml
+
+New-Item -Name pubspec.yaml -ItemType file
+@("name: $nameList", "publish_to: none", "", "environment:", "  sdk: ^3.5.1", "", "dependencies:", "  flutter:", "  flutter:", "", "  component_library:", "    path: ../../component_library", "  domain_entities:", "    path: ../../domain_entities", "  ${nameRepo}:", "    path: ../../${nameRepo}", "dev_dependencies:", "  flutter_lints: ^4.0.0") | Add-Content -Path $path\packages\features\$nameList\pubspec.yaml
+
+# Installation de la dépendance provider
+flutter pub add provider
+
+# Création des dossiers lib/src et du barrel
+mkdir lib
+Set-Location lib
+
+New-Item -Name $nameList".dart" -ItemType file
+@("library;", "", "export 'src/providers/providers.dart';", "export 'src/screens/screens.dart';", "", "export 'package:provider/provider.dart';") | Add-Content -Path $path\packages\features\$nameList\lib\$nameList".dart"
+
+mkdir src
+Set-Location src
+
+# Ajout des dossier providers et de ses fichiers
+mkdir providers
+Set-Location providers
+
+New-Item -Name "${nameList}_state.dart" -ItemType file
+@("part of '${nameList}_provider.dart';") | Add-Content -Path $path\packages\features\$nameList\lib\src\providers\${nameList}_state.dart
+
+New-Item -Name "${nameList}_provider.dart" -ItemType file
+@("part '${nameList}_state.dart';") | Add-Content -Path $path\packages\features\$nameList\lib\src\providers\${nameList}_provider.dart
+
+New-Item -Name "providers.dart" -ItemType file
+@("export '${nameList}_provider.dart';", "export '${nameList}_state.dart';") | Add-Content -Path $path\packages\features\$nameList\lib\src\providers\providers.dart
+
+Set-Location ..
+
+# Ajout des dossier screens et de ses fichiers
+mkdir screens
+Set-Location screens
+
+New-Item -Name "screens.dart" -ItemType file
